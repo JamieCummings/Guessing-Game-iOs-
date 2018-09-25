cfbead0e662cea200718203dd9854012c9f333a6
Jamie Cummings <jamiecummings@MacBook-Pro-2.local>
2018-09-25 14:46:08 -0500

    background colors added

--- Guessing-Game/OptionsPage.swift
+++ Guessing-Game/OptionsPage.swift
@@ -14,8 +14,9 @@ class OptionsPage: UIViewController {
     @IBOutlet weak var numberRangeLb: UILabel!
     @IBOutlet weak var showNumber: UILabel!
     
+    @IBOutlet weak var colorLabel: UILabel!
     var selectedNumber: Int!
-    
+    var randomColor: [UIColor] = [.black, .blue, .purple, .orange, .yellow, .red, .green, .gray]
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let viewController = segue.destination as? GuessingGameViewController {
             viewController.maxNumber = selectedNumber
@@ -23,11 +24,13 @@ class OptionsPage: UIViewController {
     }
     
     override func viewDidLoad() {
-        super.viewDidLoad()
         
+        super.viewDidLoad()
+        colorLabel.text = "Press to change background color"
         welcomeOptPg.text = "Menu"
         numberRangeLb.text = "Choose max number"
         showNumber.isHidden = true
+        
     }
     
     @IBAction func rangeSlid(_ sender: UISlider) {
@@ -35,6 +38,10 @@ class OptionsPage: UIViewController {
         selectedNumber = currentValue
         showNumber.isHidden = false
         showNumber.text = "\(currentValue)"
+        
+    }
+    @IBAction func colorButtonPress(_ sender: Any) {
+        self.view.backgroundColor = randomColor.randomElement()
         
     }
 }
