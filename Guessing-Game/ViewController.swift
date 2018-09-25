8248ab94e30ed97298945f75fc9d1a271c4c2be6
Jamie Cummings <jamiecummings@MacBook-Pro-2.local>
2018-09-25 09:26:47 -0500

    added a ton more

--- Guessing-Game/ViewController.swift
+++ Guessing-Game/ViewController.swift
@@ -17,29 +17,88 @@ class ViewController: UIViewController {
     @IBOutlet weak var guessStatusLabel: UILabel!
     @IBOutlet weak var submitButton: UIButton!
     @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var recordLabel: UILabel!
+    
+    var randomNumber = Int(arc4random_uniform(100) + 1)
+    
+    var attempts = 5
+    
+    var playerWins = 0
+    
+    var playerLosses = 0
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        attemptsLabel.text = "Attempts Remaining"
+        attemptsLabel.text = "Attempts Remaining: \(attempts)"
         greatingLabel.text =
         """
         Welcome
         to The Guessing Game
         """
-        
+        recordLabel.text = "Player Wins \(playerWins) Player Losses \(playerLosses)"
     }
 
     @IBAction func submitTapped(_ sender: Any) {
         var guess : Int? = nil
         guess = Int(guessTextField.text!)
+        
+        if guess == randomNumber {
+            
+            guessStatusLabel.text = "You win! :)"
+            guessTextField.isHidden = true
+            playerWins += 1
+            submitButton.isHidden = true
+            
+        } else if guess! > 100 || guess! < 1 {
+            
+          guessStatusLabel.text = "Please guess in the range"
+            
+        } else if guess! > randomNumber {
+            guessStatusLabel.text = "Guess is too high"
+            attempts -= 1
+            if attempts == 0 {
+                
+                greatingLabel.text = "GAME OVER!"
+                guessStatusLabel.text = "Do you want to play again? Please press Reset."
+                submitButton.isHidden = true
+                guessTextField.isHidden = true
+                
+            }
+        } else if guess! < randomNumber {
+            guessStatusLabel.text = "Guess is too low"
+            attempts -= 1
+            if attempts == 0 {
+                
+                greatingLabel.text = "GAME OVER!"
+                guessStatusLabel.text = "Do you want to play again? Please press Reset"
+                submitButton.isHidden = true
+                guessTextField.isHidden = true
+                playerLosses += 1
+                
+            }
+        }
     
         
     }
     
-    @IBAction func restPushed(_ sender: Any) {
+    @IBAction func resetPushed(_ sender: Any) {
+        
+        greatingLabel.text =
+        """
+        Welcome
+        to The Guessing Game
+        """
+        attemptsLabel.text = "Attempts Remaining: \(attempts)"
+        guessStatusLabel.text = ""
+        submitButton.isHidden = false
+        guessTextField.isHidden = false
+        attempts = 5
+        randomNumber = Int(arc4random_uniform(100) + 1)
+        
         
     }
 }
+
 
